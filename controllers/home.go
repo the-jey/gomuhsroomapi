172437b28a59f5cdb553fbaed4c1a168e64bcad9
the-jey <jey.pley@gmail.com>
2024-09-09 23:33:09 +0200

    little fun

--- controllers/home.go
+++ controllers/home.go
@@ -7,11 +7,12 @@ import (
 
 type Data struct {
 	Message string `json:"message"`
+	Creator string `json:"creator"`
 }
 
 // Hello, World handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	data := Data{Message: "Hello, World!"}
+	data := Data{Message: "Hello, World!", Creator: "THE JEY"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
